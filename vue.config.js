f30984dd5917ae5936cbac90dd38b8d7e7e70cf6
google110110 <wxuanwxwx@gmail.com>
2020-04-05 17:22:38 +0800

    one

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,33 @@
+// module.exports = {
+//     devServer: {
+//       //反向代理
+//       proxyTable: {
+//         // '/api' : {
+//         //     target : 'https://autumnfish.cn',
+//         //     changeOrigin : true,
+//         //     secure : true,
+//         //     // pathRewrith : {
+//         //     //   '/api' : '/api'
+//         //     // }
+//         // },
+//         '/api' : {
+//           target : 'https://music.163.com/api',
+//           changeOrigin : true,
+//           secure : true,
+//           pathRewrith : {
+//             '^/api' : '/api'
+//           }
+//       }
+//       }
+//     },
+//     //项目根路径问题
+//     // publicPath:'./',
+//     // baseUrl: './',
+//   }
 module.exports = {
     devServer: {
-      //反向代理
-      proxyTable: {
-        // '/api' : {
-        //     target : 'https://autumnfish.cn',
-        //     changeOrigin : true,
-        //     secure : true,
-        //     // pathRewrith : {
-        //     //   '/api' : '/api'
-        //     // }
-        // },
-        '/api' : {
-          target : 'https://music.163.com/api',
-          changeOrigin : true,
-          secure : true,
-          pathRewrith : {
-            '^/api' : '/api'
-          }
-      }
-      }
     },
     //项目根路径问题
-    // publicPath:'./',
+    publicPath:'./'
     // baseUrl: './',
   }
