83a7c33ea757bba1c22a83eec212e7d41e1e3395
google110110 <wxuanwxwx@gmail.com>
2020-04-05 18:46:32 +0800

    m46

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-    devServer: {
-    //项目根路径问题
-    // publicPath:'./',
-    // baseUrl: './',
-  }
+  devServer: {
+    // proxy: {
+    //   '/api':{
+    //       target:'http//196.128.6.23',
+    //       changeOrigin:true
+    //   }
+    // }
+  },
+  //项目根路径的问题
+  publicPath:'/my-uusic'
+  // publicPath:'./',
+  // baseUrl: './',
 }
